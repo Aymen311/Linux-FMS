a959395853342bcdf24c643fad3564ce29f33fc5
Aymen Berriche <59454072+Aymen311@users.noreply.github.com>
2022-06-06 13:07:28 +0100

    Update script.c

--- script.c
+++ script.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
 #include <string.h>
 void Liste_Disques(void);
 void Afficher_Secteur(const char * disque_physique, int Num_sect) ; 
@@ -9,12 +8,15 @@ void Lire_Secteur(unsigned char **p, const char * disque_physique, int Num_sect)
 void main(){
     char disque[256];
     char input[256];
+    int n;
     strcpy(disque, "/dev/");
     Liste_Disques();    
     printf("\nEntrez le nom du disque physique : ");
     scanf("%s", input);
+    printf("\nEntrez le numero de secteur a lire : ");
+    scanf("%d" , &n);
     strcat(disque,input);
-    Afficher_Secteur(disque, 0) ;
+    Afficher_Secteur(disque, n) ;
 }
 
 
@@ -24,8 +26,8 @@ disque physique disque_physique*/
 void Lire_Secteur(unsigned char **p, const char * disque_physique, int Num_sect)
 {
     int i, n,s;
-    unsigned char *buffer = (unsigned char*)malloc(sizeof(unsigned char)*512);
-    long int a=512*Num_sect; /* secteur 10 */
+    unsigned char buffer = (unsigned char)malloc(sizeof(unsigned char)*512);
+    long int a=512*Num_sect; 
     FILE *disk=NULL;
     disk = fopen(disque_physique, "rb"); /* Linux */
     if(disk == NULL) printf("\n Erreur le disque physique1 n'est pas ouvert\n" );
@@ -47,15 +49,7 @@ void Lire_Secteur(unsigned char **p, const char * disque_physique, int Num_sect)
 /*Afficher_Secteur (disque_physique, Num_sect): Appelle la fonction lire_secteur
 (disque_physique, Num_sect) pour lire le secteur Num_sect et affiche son contenu en
 hexadécimal.*/
-void Afficher_Secteur(const char * disque_physique, int Num_sect){
-    unsigned char *p; 
-    Lire_Secteur(&p,disque_physique, Num_sect);
-    for(int i=0; i<512; i++){
-        if(i%16==0) printf("\n %2d : ",i/16 +1 );
-        printf("%02x ", *(p+i-1));
-    }
-    printf("\n");
-}
+
 
 
 void Liste_Disques(void){
@@ -67,27 +61,38 @@ void Liste_Disques(void){
         }
         buffer[j] = 0;
     }
-    DIR* dir = opendir("/dev/");
-    if (!dir) {
-        perror("opendir");
-        exit(1);
-    }
+   
 
-    struct dirent* entry;
+   FILE* ptr;
+    char str[256];
+    char *token;
+    char buf[256];
+    char name[256];
+    ptr = fopen("/proc/partitions", "r");
+    while (fgets(str, 50, ptr) != NULL) {
+        
+        token = strtok(str, " ");
+       while( token != NULL ) {
+        strcpy(name,token);
+        token = strtok(NULL, " ");
+            }
+  slice_str(name,buf,0,1);
+  if (strlen(name) == 4 &&(!strcmp("sd",buf) || !strcmp("hd",buf))){
+              printf("%s\n",name);
+       }
 
-    printf("La liste des disques:\n");
-    while ( (entry = readdir(dir)) != NULL) 
-    {
-        char dir_name[256];
-        char buf[256];
-        strcpy(dir_name,entry->d_name);
-        slice_str(dir_name,buf,0,1);
-        if (strlen(dir_name)==3 && (!strcmp("sd",buf) || !strcmp("hd",buf)))
-        printf("%s\n", dir_name);
-    }
 
-    if (closedir(dir) == -1) {
-        perror("closedir");
-        exit(1);
+        }
+    fclose(ptr);       
+}
+
+void Afficher_Secteur(const char * disque_physique, int Num_sect){
+    unsigned char *p; 
+    Lire_Secteur(&p,disque_physique, Num_sect);
+    int j =0 ;
+    for(int i=0; i<512; i++){
+        if(i%16==0) {printf("\n %04d : ",j ); j+=16;}
+        printf("%02x ", *(p+i));
     }
+    printf("\n");
 }
